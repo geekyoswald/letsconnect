d4620705fda027000a6578ffc35340600d296bc6
Saksham5289 <f20200361@pilani.bits-pilani.ac.in>
2024-11-11 23:36:20 +0530

    Asd

--- src/app/components/right/Chats.tsx
+++ src/app/components/right/Chats.tsx
@@ -1,23 +1,36 @@
+"use client";
+
 import FriendBox from "@/app/utilcomponents/FriendBox";
 import PrimaryHeading from "@/app/utilcomponents/PrimaryHeading";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import friend1 from "../../assets/pp7.png";
-import friend2 from "../../assets/pp6.png";
-import friend3 from "../../assets/pp5.png";
-import friend4 from "../../assets/pp4.png";
-import friend5 from "../../assets/pp3.png";
+
 import friend6 from "../../assets/pp2.png";
+import axios from "axios";
 
 const Chats = () => {
+  interface Friend {
+    id: number;
+    username: string;
+  }
+
+  const [allFriends, setAllFriends] = useState<Friend[]>([]);
+
+  useEffect(() => {
+    const getFriends = async () => {
+      const allFriends = await axios.get("http://localhost:3000/api/users/all");
+      setAllFriends(allFriends.data);
+      console.log(allFriends.data);
+    };
+    getFriends();
+  }, []);
+
   return (
     <div className=" m-2 h-3/6 w-11/12 ">
       <PrimaryHeading data="Chats" />
-      <FriendBox url={friend1.src} name="John" />
-      <FriendBox url={friend2.src} name="John" />
-      <FriendBox url={friend3.src} name="John" />
-      <FriendBox url={friend4.src} name="John" />
-      <FriendBox url={friend5.src} name="John" />
-      <FriendBox url={friend6.src} name="John" />
+      {allFriends.map((friend) => {
+        return <FriendBox url={friend6.src} name={friend.username} />;
+      })}
     </div>
   );
 };
