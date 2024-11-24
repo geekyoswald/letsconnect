18a9bdac7440ccfeab1ff7624a912452d4924d51
oswald <geekyoswald@gmail.com>
2024-11-24 18:07:31 +0530

    asd

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -6,7 +6,7 @@ import RightBar from "../components/right/RightBar";
 const page = () => {
   return (
     <div className="w-screen h-screen flex justify-center bg-cyan-200 p-4">
-      <div className=" h-full w-9/12   grid grid-cols-5 rounded-2xl">
+      <div className=" h-full w-9/12   grid grid-cols-5  rounded-2xl">
         <LeftBar />
         <CenterBar />
         <RightBar />
